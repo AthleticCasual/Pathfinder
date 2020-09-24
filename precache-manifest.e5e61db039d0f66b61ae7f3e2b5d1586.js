0ebbec11a64c10c5bee39c04a4b706e6529016e4
Brandon Ta <bvt@uw.edu>
2020-09-24 16:09:04 -0700

    Updates

---
+++ precache-manifest.e5e61db039d0f66b61ae7f3e2b5d1586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a961e52ab416e9633999cd2d11130417",
+    "url": "/Pathfinder/index.html"
+  },
+  {
+    "revision": "9f3e8d4396359c8cbd41",
+    "url": "/Pathfinder/static/css/main.3c3da732.chunk.css"
+  },
+  {
+    "revision": "48084af39cecf1641217",
+    "url": "/Pathfinder/static/js/2.a85086f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pathfinder/static/js/2.a85086f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3e8d4396359c8cbd41",
+    "url": "/Pathfinder/static/js/main.7e06e05c.chunk.js"
+  },
+  {
+    "revision": "9f8fba755e0a5bd0706b",
+    "url": "/Pathfinder/static/js/runtime-main.f7faf592.js"
+  }
+]);
